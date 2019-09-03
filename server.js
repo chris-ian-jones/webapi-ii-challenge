e17a06d2a2481a9d4b36246e474bc7efe2ccf8bd
Chris Jones <chrisjones@Chriss-MacBook-Pro.local>
2019-09-03 23:52:33 +0100

    Completed MVP: added comments and tidied up code

--- server.js
+++ server.js
@@ -12,33 +12,36 @@ server.get('/', (req, res) => {
   })
 })
 
+// Returns an array of all the post objects contained in the database
 server.get('/api/posts', (req, res) => {
   db.find()
     .then(posts => {
-      console.log(posts)
       res.status(200).json({
         posts
       })
     })
     .catch(err => {
-      console.log(err)
       res.status(500).json({
         error: 'The posts information could not be retrieved.'
       })
     })
 })
 
+
+// Creates a post using the information sent inside the request body
 server.post('/api/posts', (req, res) => {
   const newPost = req.body
 
+  // check if request body is missing the title or contents properties
   if (!req.body.title || !req.body.contents) {
     res.status(400).json({
       errorMessage: 'Please provide title and contents for the post'
     })
   } else {
+    // save the new post to the database
     db.insert(newPost)
       .then(post => {
-        console.log(post)
+        // return the newly created post
         db.findById(post.id)
           .then(newPost => {
             res.status(201).json({
@@ -46,8 +49,8 @@ server.post('/api/posts', (req, res) => {
             })
           })
       })
+      // if theres an error while saving the post
       .catch(err => {
-        console.log(err)
         res.status(500).json({
           error: 'There was an error while saving the post to the database'
         })
@@ -55,20 +58,27 @@ server.post('/api/posts', (req, res) => {
   }
 })
 
+
+// Returns the post object with the specified id
 server.get('/api/posts/:id', (req, res) => {
+  // use destructuring to grab id from url
   const { id } = req.params
+  // check to see if post with id exists
   db.findById(id)
     .then(post => {
+      // if findById function evaluates to empty array send back error message
       if (post.length === 0){
         res.status(404).json({
           message: 'The post with the specified ID does not exist.'
         })
       } else {
+        // send back first (and only) post in array returned from findById function
         res.status(200).json({
           post: post[0]
         })
       }
     })
+    // if theres an error in retrieving the post from the database
     .catch(err => {
       res.status(500).json({
         error: 'The post information could not be retrieved'
@@ -76,20 +86,27 @@ server.get('/api/posts/:id', (req, res) => {
     })
 })
 
+// Returns an array of all the comment objects associated with the post with the specified id
 server.get('/api/posts/:id/comments', (req, res) => {
+  // use destructuring to grab id from url
   const { id } = req.params
+  // check to see if post with id exists
   db.findById(id).then(post => {
+    // if findById function evaluates to empty array send back error message
     if (post.length === 0){
       res.status(404).json({
         message: 'The post with the specified ID does not exist.'
       })
     } else {
+      // search for comments associated with the post with the specified id,
+      // return an array of all the comment objects
       db.findPostComments(id)
         .then(comments => {
           res.status(200).json({
             comments
           })
         })
+        // send error message if theres an error in retrieving the comments from the database
         .catch(err => {
           res.status(500).json({
             error: 'The comments information could not be retrieved.'
@@ -99,25 +116,35 @@ server.get('/api/posts/:id/comments', (req, res) => {
   }) 
 })
 
+
+// Creates a comment for the post with the specified id using information sent inside of the request body
 server.post('/api/posts/:id/comments', (req, res) => {
+  // use destructuring to grab id from url
   const { id } = req.params
+  // check to see if post with id exists
   db.findById(id)
     .then(post => {
+      // if findById function evaluates to empty array send back error message
       if (post.length === 0){
         res.status(404).json({
           message: 'The post with the specified ID does not exist.'
         })
+      // check if request body is missing the text property
       } else if (!req.body.text) {
         res.status(400).json({
           errorMessage: 'Please provide text for the comment.'
         })
       } else {
+        // create object with correct key/value pairs
         const newComment = {
           text: req.body.text,
           post_id: id
         }
+        // insert newly created object in to database
         db.insertComment(newComment)
           .then(comment => {
+            // search for newly created comment, by id returned from process of inserting new comment
+            // send back the newly created comment
             db.findCommentById(comment.id)
               .then(newComment => {
                 res.status(201).json({
@@ -125,6 +152,7 @@ server.post('/api/posts/:id/comments', (req, res) => {
                 })
               })
           })
+          // send an error message if theres an error while saving the comment
           .catch(err => {
             res.status(500).json({
               error: 'There was an error while saving the comment to the database.'
@@ -134,21 +162,28 @@ server.post('/api/posts/:id/comments', (req, res) => {
     })
 })
 
+
+// Removes the post with the specified id and returns the deleted post object. You may need to make additional calls to the database in order to satisfy this requirement
 server.delete('/api/posts/:id', (req, res) => {
+  // use destructuring to grab id from url
   const { id } = req.params
+  // check to see if post with id exists
   db.findById(id)
     .then(post => {
+      // if findById function evaluates to empty array send back error message
       if (post.length === 0){
         res.status(404).json({
           message: 'The post with the specified ID does not exist.'
         })
       } else {
+        // delete the post associated with the id from the database
         db.remove(id)
           .then(removed => {
             res.status(200).json({
               message: 'post deleted'
             })
           })
+          // send an error message if theres an error while deleting the post
           .catch(err => {
             res.status(500).json({
               error: 'The post could not be removed'
@@ -158,28 +193,38 @@ server.delete('/api/posts/:id', (req, res) => {
     })
 })
 
+
+// Updates the post with the specified id using data from the request body. Returns the modified document, NOT the original
 server.put('/api/posts/:id', (req, res) => {
+  // use destructuring to grab id from url
   const { id } = req.params
+  // check to see if post with id exists
   db.findById(id)
     .then(post => {
+      // if findById function evaluates to empty array send back error message
       if (post.length === 0){
         res.status(404).json({
           message: 'The post with the specified ID does not exist.'
         })
+      // check if request body is missing the title or contents properties
       } else if (!req.body.title || !req.body.contents) {
         res.status(400).json({
           errorMessage: 'Please provide title and contents for the post'
         })
       } else {
+        // update the new post in the database
         db.update(id, req.body)
           .then(updatedPost => {
+            // search for newly updated post by id
             db.findById(id)
+              // send back first (and only) post in array returned from findById function
               .then(post => {
                 res.status(200).json({
                   post: post[0]
                 })
               })
           })
+          // send an error message if theres an error while modifying the post
           .catch(err => {
             res.status(500).json({
               error: 'The post information could not be modified'
