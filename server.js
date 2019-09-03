f69b860586ac9e96912ec5ab0c68824fc05aee00
Chris Jones <chrisjones@Chriss-MacBook-Pro.local>
2019-09-03 20:48:30 +0100

    WIP: setup GET enpoint for /api/posts

--- server.js
+++ server.js
@@ -2,12 +2,30 @@ const express = require('express')
 
 const server = express()
 
+const db = require('./data/db.js')
+
 server.use(express.json())
 
 server.get('/', (req, res) => {
   res.status(200).json({
     api: 'up...'
   })
+})
+
+server.get('/api/posts', (req, res) => {
+  db.find()
+    .then(posts => {
+      console.log(posts)
+      res.status(200).json({
+        posts
+      })
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({
+        error: 'The posts information could not be retrieved.'
+      })
+    })
 })
 
 module.exports = server
